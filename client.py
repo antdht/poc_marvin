78c8060b0da962528d6ea490e72804155924a83f
honzhepo <znvk34@gmail.com>
2025-04-23 19:35:24 +0200

    feat: deletion of server and real implementation to finding valid padding
    Since the group have decided to do everything in one file I've deleted
    the server file and created changes the client one we will be rename
    later. In it, based on Everlasting ROBOTS paper I started implementing
    the code.
    
    There is for the moment the oracle. It is here to answer if the padding
    is good or not. And the valid padding finder which find out the good
    padding for the code.

--- client.py
+++ client.py
@@ -1,26 +1,57 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
-import base64
+from math import ceil
 import rsaTools as rsa
-import requests
+
+
+def oracle(msg: bytes) -> bool:
+    """
+    Check PKCS1v15 padding, it is here to imit the case of a server,
+    we will treat it like a black box piece of code.
+    Args:
+        msg: the message to check.
+    Returns:
+       bool: true if everything if fine, else false.
+    """
+    # PKCS#1 v1.5: 0x00 | 0x02 | PS…| 0x00
+    if len(msg) < 11 or msg[0:2] != b"\x00\x02":
+        return False
+    # find 0x00 separator after padding
+    try:
+        sep = msg.index(b"\x00", 2)
+    except ValueError:
+        return False
+    return sep >= 10  # at least 8 bytes of PS
+
+
+def find_valid_padding(ciphertext: bytes, n: int, e: int, B: int) -> int:
+    """
+    Test out padding by making call to the oracle
+    Args:
+        ciphertext: the message encrypted we try to find out
+        n: the Modulus
+        e: the exposure
+    Returns:
+        s: a valid padding
+    """
+    m = int.from_bytes(ciphertext, byteorder="big")
+    s = ceil(n / (3 * B))
+    k = (n.bit_length() + 7) // 8
+    while True:
+        c_prime = (m * pow(s, e, n)) % n
+        c_prime_bytes = c_prime.to_bytes(k, byteorder="big")
+        if oracle(c_prime_bytes) == True:
+            return s
+        s = s + 1
+
 
 # --- CONFIG ---
-GET_PUBLIC_KEY_URL = "http://localhost:8000/public-key"
-POST_MESSAGE_URL = "http://localhost:8000/decrypt"
 MESSAGE = b"Hello, RSA-secured server!"
-# ---------------
 
-# Step 1: Get the public key from server
-response = requests.get(GET_PUBLIC_KEY_URL)
-pem_str = response.json()["public_key"]
 
-# Safe load
-public_key = serialization.load_pem_public_key(
-    pem_str.encode("utf-8"), backend=default_backend()
-)
-assert isinstance(public_key, RSAPublicKey), "Invalid public key type"
-print("Public Key:", public_key)
+private_key, public_key = rsa.generateRSAKeyPair()
+
+
+cipher = rsa.encrypt(MESSAGE, public_key)
+
 
 public_numbers = public_key.public_numbers()
 n = public_numbers.n
@@ -37,5 +68,5 @@ B = pow(2, k - 16)
 print(f"B number (B): {B}")
 
 
-def find_valid_padding(ciphertext: bytes):
-    pass
+padding = find_valid_padding(cipher, n, e, B)
+print(f"padding: {padding}")
