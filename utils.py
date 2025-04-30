47569bf388666941588a2a9f36ab1344f5c1d045
antdht <gzavier@proton.me>
2025-04-30 18:23:54 +0200

    temp: temporary fix for outliers

--- utils.py
+++ utils.py
@@ -36,7 +36,7 @@ def generate_interval(
     invalidPad = []
     publicKey = cast(rsa.RSAPublicKey, private_key.public_key())
     # Start loop
-    j = 20000000
+    j = 2000000
     print(f"testing for {j} iterations")
     for i in range(j):
         ciphertext = rsaTools.encrypt(original_plain.encode(), publicKey)
@@ -44,22 +44,37 @@ def generate_interval(
 
         if i < j // 3:
             invalidCipher = ciphertext[1:]
-            invalidPad.append(oracle_time_check(invalidCipher, private_key))
+            invalidPad.append(
+                min(
+                    oracle_time_check(invalidCipher, private_key),
+                    oracle_time_check(invalidCipher, private_key),
+                )
+            )
 
         elif i < 2 * j // 3:
             invalidCipher = ciphertext
             invalidCipher = b"\x10\x10" + invalidCipher[:2]
-            invalidPad.append(oracle_time_check(invalidCipher, private_key))
+            invalidPad.append(
+                min(
+                    oracle_time_check(invalidCipher, private_key),
+                    oracle_time_check(invalidCipher, private_key),
+                )
+            )
         else:
             invalidCipher = ciphertext[1:]
-            invalidPad.append(oracle_time_check(invalidCipher, private_key))
+            invalidPad.append(
+                min(
+                    oracle_time_check(invalidCipher, private_key),
+                    oracle_time_check(invalidCipher, private_key),
+                )
+            )
 
     print("Valid padding records:")
     validPad.sort()
     validSize = len(validPad)
     validMean = sum(validPad) / validSize
     print(
-        f"Smallest: {validPad[0]}\nMedian:{validPad[validSize // 2]}\nLargest: {validPad[-2]}\nMean: {validMean}\n"
+        f"Smallest: {validPad[0]}\nMedian: {validPad[validSize // 2]}\nLargest: {validPad[-2]}\nMean: {validMean}\n"
     )
 
     counts, bin_edges = np.histogram(
@@ -71,7 +86,7 @@ def generate_interval(
     plt.title("Log-Scale Distribution (Line Plot)")
     plt.xlabel("Value")
     plt.ylabel("Frequency (log scale)")
-    plt.savefig("valid_log_larger.png", dpi=300)
+    plt.savefig("valid_log_double.png", dpi=300)
     plt.clf()
 
     print("Invalid padding records:")
@@ -79,7 +94,7 @@ def generate_interval(
     invalidSize = len(invalidPad)
     invalidMean = sum(invalidPad) / invalidSize
     print(
-        f"Smallest: {invalidPad[0]}\nMedian:{invalidPad[invalidSize // 2]}\nLargest: {invalidPad[-2]}\nMean: {invalidMean}\n"
+        f"Smallest: {invalidPad[0]}\nMedian: {invalidPad[invalidSize // 2]}\nLargest: {invalidPad[-2]}\nMean: {invalidMean}\n"
     )
     counts2, bin_edges2 = np.histogram(
         invalidPad, bins=100, range=(invalidPad[0], invalidPad[-1])
@@ -90,6 +105,6 @@ def generate_interval(
     plt.title("Log-Scale Distribution (Line Plot)")
     plt.xlabel("Value")
     plt.ylabel("Frequency (log scale)")
-    plt.savefig("unvalid_log_larger.png", dpi=300)
+    plt.savefig("unvalid_log_double.png", dpi=300)
 
     return (0.0, 1.0), (1.0, 2.0)
