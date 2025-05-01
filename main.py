4070336efab70847b96eddd08a09114120aa6304
antdht <gzavier@proton.me>
2025-05-01 15:21:04 +0200

    feat: added bones for the exec loop

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 import rsaTools
-import utils
 from cryptography.hazmat.primitives.asymmetric import rsa
 from typing import cast
 
 
-# --- CONFIG ---
-MESSAGE = "Hello, RSA-secured server!"
-private_key, public_key = rsaTools.generateRSAKeyPair()
-cipher = rsaTools.encrypt(MESSAGE.encode(), public_key)
-public_numbers = cast(rsa.RSAPublicNumbers, public_key.public_numbers())
-n = public_numbers.n
-e = public_numbers.e
-# --- END CONFIG ---
-#
-# print(f"Modulus (n): {n}")
-# print(f"Exponent (e): {e}")
-#
-# k = n.bit_length()
-# print(f"K number (k): {k}")
-#
-# B = pow(2, k - 16)
-#
-# print(f"B number (B): {B}")
-
-utils.generate_interval(private_key, MESSAGE)
+def marvin_break(cipherText: bytes, private_key: rsa.RSAPrivateKey):
+    public_key = cast(rsa.RSAPublicKey, private_key.public_key())
+    public_numbers = cast(rsa.RSAPublicNumbers, public_key.public_numbers())
+    n = public_numbers.n
+    e = public_numbers.e
+    pass
 
 
-# M = [2 * B, 3 * B - 1]
+if __name__ == "__main__":
+    # --- CONFIG ---
+    MESSAGE = "Hello, RSA-secured server!"
+    private_key, public_key = rsaTools.generateRSAKeyPair()
+    cipher = rsaTools.encrypt(MESSAGE.encode(), public_key)
